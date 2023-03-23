2424ab6e9a894dc3e161a133c687134d0d3e57e8
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-03-23 19:22:27 +0200

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/calculator';
+import RandomQuote from './components/quotes';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <RandomQuote />
     </div>
   );
 }
