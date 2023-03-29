739fa85fb6869fb1056aa92391396275f7fd35f8
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-03-29 16:05:13 +0200

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
-import Calculator from './components/calculator';
-import RandomQuote from './components/quotes';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './routes/Home';
+import MyCalc from './routes/CalcPage';
+import Quote from './routes/quote';
+import NotMatch from './routes/NotMatch';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <RandomQuote />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="calculator" element={<MyCalc />} />
+        <Route path="quote" element={<Quote />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
