b03f0adeffea9e4af21158fae15feeb198fda8fe
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-03-29 16:11:45 +0200

    Add calculate route

---
+++ src/routes/CalcPage.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import Calculator from '../components/calculator';
+
+function MyCalc() {
+  return (
+    <div className="#">
+      <Calculator />
+    </div>
+  );
+}
+
+export default MyCalc;
